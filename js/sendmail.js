0b6f364205fba6f4c6c8b0c3dcfa2434d655dab6
Agustin Polo <poloagustin@gmail.com>
2016-07-30 18:29:19 -0300

    arreglos en landing

--- js/sendmail.js
+++ js/sendmail.js
@@ -1,10 +1,15 @@
-function onSuccess (ferror){
-  if (ferror) {
-    return false;
-  } else {
-    $("#successmsg").addClass("show");
-    $('#send-button').prop('disabled', true);
-    emailjs.sendForm('default_service', 'primer_contacto', this);
-    return false;
-  }
+function onSuccess(ferror, form) {
+    if (ferror) {
+        return false;
+    } else {
+        emailjs.sendForm('default_service', 'primer_contacto', form).then(
+            function () {
+                $("#successmsg").addClass("show");
+                $('#send-button').prop('disabled', true);
+            },
+            function (error) {
+                alert('Hubo un error al enviar el mail.');
+            });
+        return false;
+    }
 }
--- js/validate.js
+++ js/validate.js
@@ -1,57 +1,58 @@
 /*global jQuery:false */
 jQuery(document).ready(function ($) {
-  "use strict";
+    "use strict";
 
-//Contact
-  $('form.contactForm').submit(function () {
-		console.log("entró submit");
-    var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    //Contact
+    $('form.contactForm').submit();
+});
+
+function onSubmit(form) {
+    console.log("entró submit");
+    var f = $(form).find('.form-group'),
+        ferror = false,
+        emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function () { // run all inputs
-      var i = $(this),//current input
-        rule = i.attr('data-rule');
+        var i = $(form),//current input
+            rule = i.attr('data-rule');
 
-      if (rule !== undefined) {
-        var ierror = false,
-          pos = rule.indexOf(':', 0); // error flag for current input
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
+        if (rule !== undefined) {
+            var ierror = false,
+                pos = rule.indexOf(':', 0); // error flag for current input
+            if (pos >= 0) {
+                var exp = rule.substr(pos + 1, rule.length);
+                rule = rule.substr(0, pos);
+            } else {
+                rule = rule.substr(pos + 1, rule.length);
+            }
+
+            switch (rule) {
+                case 'required':
+                    if (i.val() === '') { ferror = ierror = true; }
+                    break;
+
+                case 'maxlen':
+                    if (i.val().length < parseInt(exp)) { ferror = ierror = true; }
+                    break;
+
+                case 'email':
+                    if (!emailExp.test(i.val())) { ferror = ierror = true; }
+                    break;
+
+                case 'checked':
+                    if (!i.attr('checked')) { ferror = ierror = true; }
+                    break;
+
+                case 'regexp':
+                    exp = new RegExp(exp);
+                    if (!exp.test(i.val())) { ferror = ierror = true; }
+                    break;
+            }
+            i.next('.validation').html((
+                ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : ''
+            )).show('blind');
         }
-
-        switch (rule) {
-        case 'required':
-          if (i.val() === '') {ferror = ierror = true; }
-          break;
-
-        case 'maxlen':
-          if (i.val().length < parseInt(exp)) { ferror = ierror = true; }
-          break;
-
-        case 'email':
-          if (!emailExp.test(i.val())) { ferror = ierror = true; }
-          break;
-
-        case 'checked':
-          if (!i.attr('checked')) { ferror = ierror = true; }
-          break;
-
-        case 'regexp':
-          exp = new RegExp(exp);
-          if (!exp.test(i.val())) { ferror = ierror = true; }
-          break;
-        }
-        i.next('.validation').html((
-          ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : ''
-        )).show('blind');
-      }
     });
-    console.log(ferror, this);
-    return onSuccess(ferror, $(this));
-
-  });
-});
+    console.log(ferror, form);
+    return onSuccess(ferror, form);
+}
